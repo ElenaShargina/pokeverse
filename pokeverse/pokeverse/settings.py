fa60fd6249801a92117204fcee91c526cd8a5923
user <elena.shargina@gmail.com>
2023-08-16 15:19:04 +0300

    allowed_hosts fixed

--- pokeverse/pokeverse/settings.py
+++ pokeverse/pokeverse/settings.py
@@ -17,18 +17,22 @@ import environ
 # Initialise environment variables
 env = environ.Env(
     # set casting, default value
-    DEBUG=(bool, False)
+    DEBUG=(bool, False),
 )
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 # False if not in os.environ because of casting above
 DEBUG = env('DEBUG')
 
-# Raises Django's ImproperlyConfigured
-# exception if SECRET_KEY not in os.environ
 SECRET_KEY = env('SECRET_KEY')
+
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+
 
 # Application definition
 
