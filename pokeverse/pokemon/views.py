5bf1e4fe3d8e57afd2684544cb5b28ccaf10f9a2
elena <elena.shargina@gmail.com>
2023-09-18 12:50:20 +0300

    Fixed bug with not showing Pokemon as collected in views Ability_detail, species_detail, typepokemon_detail , pokemon_detail

--- pokeverse/pokemon/views.py
+++ pokeverse/pokemon/views.py
@@ -8,6 +8,22 @@ from django.views.generic.edit import UpdateView
 from django.db.models import Q
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+class CollectionInfoMixin():
+    def add_collection_info(self, context):
+        # если пользователь залогинен, накладываем информацию про его коллекцию
+        user = self.request.user
+        if user.is_authenticated and context.get('object_list', None):
+            user_collection = user.get_or_create_collection()[0].pokemons.all()
+            object_list_ids = []
+            if type(context['object_list'][0]) == Pokemon:
+                object_list_ids = [p.id for p in context['object_list']]
+            elif type(context['object_list'][0]) == dict:
+                object_list_ids = [p['id'] for p in context['object_list']]
+            context['in_collection'] = {key: False for key in object_list_ids}
+            for p in object_list_ids:
+                if user_collection.filter(id = p):
+                    context['in_collection'][p] = True
+
 class MainIndex(generic.View):
     template_name = 'main.html'
 
@@ -31,26 +47,17 @@ class SearchResultsView(generic.ListView):
         )
         return {'query':query,'pokemon_list':pokemon_list, 'ability_list':ability_list,'types_list':types_list}
 
-class PokemonIndexView(generic.ListView):
+class PokemonIndexView(generic.ListView, CollectionInfoMixin):
     model = Pokemon
     paginate_by = 60
     ordering = ['name']
     
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(object_list=object_list, **kwargs)
-        # если пользователь залогинен, накладываем информацию про его коллекцию
-        user = self.request.user
-        if user.is_authenticated:
-            user_collection = user.get_or_create_collection()[0].pokemons.all()
-            context['in_collection'] = {key:False for key in [p.id for p in context['object_list']] }
-            for p in context['object_list']:
-                if p in user_collection:
-                    context['in_collection'][p.id] = True
+        self.add_collection_info(context)
         return context
 
-
-
-class PokemonDetailView(generic.DetailView):
+class PokemonDetailView(generic.DetailView, CollectionInfoMixin):
     model = Pokemon
 
     def get_context_data(self, **kwargs):
@@ -63,7 +70,8 @@ class PokemonDetailView(generic.DetailView):
             'Базовый опыт': context['object'].base_experience,
         }
         if context['object'].species!=None:
-            context['pokemons'] = context['object'].species.pokemons.exclude(pokeapi_id=context['object'].pokeapi_id)
+            context['object_list'] = context['object'].species.pokemons.exclude(pokeapi_id=context['object'].pokeapi_id)
+            self.add_collection_info(context)
         return context
 
 
@@ -71,7 +79,7 @@ class AbilityIndexView(generic.ListView):
     model = Ability
 
 
-class AbilityDetailView(generic.DetailView):
+class AbilityDetailView(generic.DetailView, CollectionInfoMixin):
     model = Ability
 
     def get_context_data(self, **kwargs):
@@ -81,14 +89,15 @@ class AbilityDetailView(generic.DetailView):
             'PokeApi ID': context['object'].pokeapi_id,
             'Полное описание': context['object'].effect_entry,
         }
-        context['pokemons'] = [{'id': x.id, 'name': x.name, 'image_big': x.image_big} for x in
+        context['object_list'] = [{'id': x.id, 'name': x.name, 'image_big': x.image_big} for x in
                                context['object'].pokemons.all()]
+        self.add_collection_info(context)
         return context
 
 class SpeciesPokemonIndexView(generic.ListView):
     model = SpeciesPokemon
 
-class SpeciesPokemonDetailView(generic.DetailView):
+class SpeciesPokemonDetailView(generic.DetailView, CollectionInfoMixin):
     model = SpeciesPokemon
 
     def get_context_data(self, **kwargs):
@@ -97,19 +106,21 @@ class SpeciesPokemonDetailView(generic.DetailView):
             'Название': context['object'].name,
             'Текст': context['object'].text,
         }
-        context['pokemons'] = [{'id': x.id, 'name': x.name, 'image_big': x.image_big} for x in
+        context['object_list'] = [{'id': x.id, 'name': x.name, 'image_big': x.image_big} for x in
                                context['object'].pokemons.all()]
+        self.add_collection_info(context)
         return context
 
 class TypePokemonIndexView(generic.ListView):
     model = TypePokemon
 
-class TypePokemonDetailView(generic.DetailView):
+class TypePokemonDetailView(generic.DetailView, CollectionInfoMixin):
     model = TypePokemon
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['pokemons'] = [{'id': x.id, 'name': x.name, 'image_big': x.image_big} for x in
+        context['object_list'] = [{'id': x.id, 'name': x.name, 'image_big': x.image_big} for x in
                                context['object'].pokemons.all()]
-        context['len_pokemons'] = str(len(context['pokemons']))
+        context['len_pokemons'] = str(len(context['object_list']))
+        self.add_collection_info(context)
         return context
